cf4c9d78ef6af5cb9f09a31d7420dee908d51df5
teamanubot <rivaimunthe02@gmail.com>
2024-03-27 20:40:50 +0700

    Contoh script Python

---
+++ pert2/pert2.py
@@ -0,0 +1,22 @@
+print ("hello world!")
+
+a = 3
+b = 2
+
+print (a+b)
+
+nama = "Rivai"
+
+print (nama)
+
+z = [1, 2, 3]
+
+#### Cara mengetahui tipe variabel ###
+print (type(a))
+print (type(nama))
+print (type(z))
+
+#### Cara memeriksa tipe Variabel ###
+print (isinstance(a, int))
+print (isinstance(nama, int))
+print (isinstance(nama, str))
