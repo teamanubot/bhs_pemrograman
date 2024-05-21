91f9a6a3ae7ae7ffce823ddd954710a281b3e489
teamanubot <rivaimunthe02@gmail.com>
2024-05-21 10:10:54 +0700

    Jawban UTS Bahasa Pemrograman

---
+++ UTS/nomor 4.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int umur_user = 21;
+    cout << "Berarti Usia Kalian sekarang adalah " << umur_user << " Tahun" << endl;
+    return 0;
+}
